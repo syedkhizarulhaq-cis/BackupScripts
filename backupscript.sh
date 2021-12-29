9f1067173a9f2edf5cb5d1fdb6505b2d0ee09505
nullb0i <syedkhizarulhaq@yahoo.com>
2021-12-29 15:37:43 +0500

    added elif part

--- backupscript.sh
+++ backupscript.sh
@@ -4,12 +4,15 @@ var1=`ls -Asrt /var/opt/gitlab/backups | tail -n 1 | cut -d ' ' -f 1` #Get the f
 var2=`ls -Asrt /var/opt/gitlab/backups | tail -n 2 | head -n 1 | cut -d ' ' -f 1` #Get the file size of the 2nd last file
 var3=`ls -Artls /var/opt/gitlab/backups/ | tail -n 2 | head -n 1 | cut -d ' ' -f 10 | tr -d ':'` #Get file creation timestamp of second last file 
 var4=`ls -Artls /var/opt/gitlab/backups/ | tail -n 1 | cut -d ' ' -f 10 | tr -d ':'` #Get file creation timestamp of last file
+var5=`ls -Art /var/opt/gitlab/backups/ | tail -n 2 | head -n 1` #Get second last file
+var6=`ls -Art /var/opt/gitlab/backups/ | tail -n 1` #Get last file
 if ([ $var1 -eq $var2 ] && [ $var4 -gt $var3 ]); then #If both file size are same and file creation timestamp of last file is greater than second last file
-	var5=`ls -Art /var/opt/gitlab/backups/ | tail -n 2 | head -n 1` #Get second last file
-	var6=`ls -Art /var/opt/gitlab/backups/ | tail -n 1` #Get last file
-	rm -f /var/opt/gitlab/backups/$var5 #remove second last file
-else
-	rm -f /var/opt/gitlab/backups/$var6 #remove last file
 
+	rm -f /var/opt/gitlab/backups/$var5 #remove second last file
+elif ([ $var1 -eq $var2 ] && [ $var3 -gt $var4 ]); then
+
+	rm -f /var/opt/gitlab/backups/$var6 #remove last file
+else
+	echo "Do nothing"
 fi
 
