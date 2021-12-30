5f0cf5f94025741cfaf682c7872037e7ecb5672a
nullb0i <syedkhizarulhaq@yahoo.com>
2021-12-30 14:14:46 +0500

    Added finalbackupscript file

--- backupscript.sh
+++ backupscript.sh
@@ -6,6 +6,7 @@ var3=`ls -Artls /var/opt/gitlab/backups/ | tail -n 2 | head -n 1 | cut -d ' ' -f
 var4=`ls -Artls /var/opt/gitlab/backups/ | tail -n 1 | cut -d ' ' -f 10 | tr -d ':'` #Get file creation timestamp of last file
 var5=`ls -Art /var/opt/gitlab/backups/ | tail -n 2 | head -n 1` #Get second last file
 var6=`ls -Art /var/opt/gitlab/backups/ | tail -n 1` #Get last file
+
 if ([ $var1 -eq $var2 ] && [ $var4 -gt $var3 ]); then #If both file size are same and file creation timestamp of last file is greater than second last file
 
 	rm -f /var/opt/gitlab/backups/$var5 #remove second last file
