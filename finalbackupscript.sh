b6861e7bd8017074f65e697c95bda165eb65412e
syedkhizarulhaq-cis <syedkhizarulhaq@yahoo.com>
2021-12-31 09:49:57 +0500

    Changes from ot to gt and nt to lt

--- finalbackupscript.sh
+++ finalbackupscript.sh
@@ -7,15 +7,16 @@ var2=`ls -Asrt ${base_path} | tail -n 2 | head -n 1 | cut -d ' ' -f 1` #Get the 
 var3=`ls ${base_path} | tail -n 2 | head -n 1` #Get second last file
 var4=`ls ${base_path} | tail -n 1` #Get last file
 
-if ( [ $var1 -eq $var2 ] && [ `stat -c %Y $base_path/$var3` -nt `stat -c %Y $base_path/$var4` ] ); then
+if ([ $var1 -eq $var2 ] && [ `stat -c %Y $base_path/$var3` -lt `stat -c %Y $base_path/$var4` ]); then
 
-	rm -f ${base_path}/$var4
+	rm -f ${base_path}/$var3
 
-elif ( [ $var1 -eq $var2 ] && [ `stat -c %Y $base_path/$var3` -ot `stat -c %Y $base_path/$var4` ] ); then
+elif ([ $var1 -eq $var2 ] && [ `stat -c %Y $base_path/$var4` -lt `stat -c %Y $base_path/$var3` ]); then
 
-        rm -f ${base_path}/$var3
+        rm -f ${base_path}/$var4
 
 else
+
 	echo "Do nothing";
 
 fi
