10e33af23c47fe5cae1e3635d3828a11298606c5
syedkhizarulhaq-cis <syedkhizarulhaq@yahoo.com>
2022-01-04 16:36:24 +0500

    comapare files in windows share and backup folder

--- movetowin.sh
+++ movetowin.sh
@@ -3,13 +3,13 @@
 count=$( ping -c 1 172.16.16.102 | grep icmp* | wc -l )
 
 
-if [ $count -gt 0 ]
+if ([ $count -gt 0 ] && [ `ls /var/opt/gitlab/backups/ | tail -n 1` == `ls /mnt/win_share | tail -n 1` ])
 then
 
-    cp /var/opt/gitlab/backups/$(ls /var/opt/gitlab/backups/ | tail -n 1) /mnt/win_share
+    echo "Do nothing"
 
 else
 
-    echo "Do nothing"
+    cp /var/opt/gitlab/backups/$(ls /var/opt/gitlab/backups/ | tail -n 1) /mnt/win_share
 
 fi
